ece2037d382f1c360c46ad353b5e059b070275aa
AndreyMolochko <Molochko_AG_16@student.grsu.by>
2018-08-25 09:05:55 +0300

    finish service

--- app/src/main/java/com/example/user/roomandroid/MainActivity.java
+++ app/src/main/java/com/example/user/roomandroid/MainActivity.java
@@ -1,30 +1,34 @@
 package com.example.user.roomandroid;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.arch.persistence.room.Room;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
-import com.example.user.roomandroid.room.AppDatabase;
-import com.example.user.roomandroid.room.bd.Bio;
-import com.example.user.roomandroid.room.bd.Company;
-import com.example.user.roomandroid.room.bd.Location;
-import com.example.user.roomandroid.room.bd.Person;
-import com.example.user.roomandroid.room.bd.Work;
-import com.example.user.roomandroid.service.foreground.MyForegroundService;
+
+import com.example.user.roomandroid.service.foreground.MyService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,150 +40,59 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    final String LOG_TAG = "myLogs";
-    /*@BindView(R.id.editTextFirstSkill)
-    EditText editTextFirstSkill;
-    @BindView(R.id.editTextSecondSkill)
-    EditText editTextSecondSkill;
-    @BindView(R.id.editTextPosition)
-    EditText editTextPosition;
-    @BindView(R.id.editTextExperience)
-    EditText editTextExperience;
-    @BindView(R.id.editTextTechno)
-    EditText editTextTechno;
-    @BindView(R.id.editTextAge)
-    EditText editTextAge;
-    @BindView(R.id.editTextSex)
-    EditText editTextSex;
-    @BindView(R.id.editTextName)
-    EditText editTextName;
-    @BindView(R.id.editTextSurname)
-    EditText editTextSurname;
-    @BindView(R.id.editTextCity)
-    EditText editTextCity;
-    @BindView(R.id.editTextCountry)
-    EditText editTextCountry;
-    @BindView(R.id.editTextStreet)
-    EditText editTextStreet;*/
-    //AppDatabase db;
-    private FloatingActionButton fab;
-    /*private String firstSkill;
-    private String secondSkill;
-    private String position;
-    private String experience;
-    private String technology;
-    private String age;
-    private String sex;
-    private String name;
-    private String surname;
-    private String city;
-    private String street;
-    private String country;*/
+    public final static String FILE_NAME = "filename";
+    private static LocationManager locationManager;
 
+    public static boolean checkGPS(){
+        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))return true;
+        else return false;
+    }
 
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_foreground_service);
-        ButterKnife.bind(this);
-        setTitle("dev2qa.com - Android Foreground Service Example.");
-        Log.i(LOG_TAG,"after init");
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        TextView tv = (TextView) findViewById(R.id.tv);
+
+        Intent intent = getIntent();
+
+        String fileName = intent.getStringExtra(FILE_NAME);
+        if (!TextUtils.isEmpty(fileName))
+            tv.setText(fileName);
+
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
     }
 
-    private void init(){
-        initView();
-        initOnClickFloatingButton();
+    public void onClickStart(View v) {
+        Log.i("dadadadada","onClickStart");
+        startService(new Intent(this, MyService.class));
     }
 
-    private void initView(){
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        fab = (FloatingActionButton) findViewById(R.id.fab);
+    public void onClickStop(View v) {
+        Log.i("dadadadada","onStop");
+        stopService(new Intent(this, MyService.class));
     }
 
-    private void initOnClickFloatingButton(){
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                /*Person person = new Person();
-                Work work = new Work();
-                Company company = new Company();
-                Bio bio = new Bio();
-                Location location = new Location();
-                List<String> skills = new ArrayList<>();
-                firstSkill = editTextFirstSkill.getText().toString();
-                secondSkill = editTextSecondSkill.getText().toString();
-                position = editTextPosition.getText().toString();
-                experience = editTextExperience.getText().toString();
-                technology = editTextTechno.getText().toString();
-                age = editTextAge.getText().toString();
-                sex = editTextSex.getText().toString();
-                name = editTextName.getText().toString();
-                surname = editTextSurname.getText().toString();
-                city = editTextCity.getText().toString();
-                street = editTextStreet.getText().toString();
-                country = editTextCountry.getText().toString();
-                skills.add(firstSkill);
-                skills.add(secondSkill);
-                company.setPosition(position);
-                company.setPosition(experience);
-                company.setPosition(technology);
-                work.setCompany(company);
-                work.setSkills(skills);
-                location.setCity(city);
-                location.setStreet(street);
-                location.setCountry(country);
-                bio.setDateOfBirthday(Integer.parseInt(age));
-                bio.setName(name);
-                bio.setSex(sex);
-                bio.setSurname(surname);
-                person.setWork(work);
-                person.setBio(bio);
-                person.setLocation(location);
-                Log.i(LOG_TAG,"before insert, size = "+db.getPersonDao().getAllPerson().size());
-                db.getPersonDao().insertPersons(person);
-                Log.i(LOG_TAG,"after insert, size = "+db.getPersonDao().getAllPerson().size());
-                List<Person>men = db.getPersonDao().getAllPerson();
-                for(int i=0;i<men.size();i++){
-                    Log.i(LOG_TAG,men.get(i).getWork().getSkills().get(0));
-                }*/
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
             }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            // other 'case' lines to check for other
+            // permissions this app might request
         }
-        return super.onOptionsItemSelected(item);
     }
 
-    @OnClick(R.id.start_foreground_service_button)
-    public void clickOnStart(){
-        Log.i(LOG_TAG,"clickonStart");
-        Intent intent = new Intent(MainActivity.this, MyForegroundService.class);
-        intent.setAction(MyForegroundService.ACTION_START_FOREGROUND_SERVICE);
-        startService(intent);
-    }
-
-    @OnClick(R.id.stop_foreground_service_button)
-    public void clickOnStop(){
-        Intent intent = new Intent(MainActivity.this,MyForegroundService.class);
-        intent.setAction(MyForegroundService.ACTION_STOP_FOREGROUND_SERVICE);
-        startService(intent);
-    }
 }
---
+++ app/src/main/java/com/example/user/roomandroid/service/foreground/MyService.java
@@ -0,0 +1,118 @@
+package com.example.user.roomandroid.service.foreground;
+
+import java.util.concurrent.TimeUnit;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+
+import com.example.user.roomandroid.MainActivity;
+import com.example.user.roomandroid.R;
+
+/**
+ * Created by User on 24.08.2018.
+ */
+
+public class MyService extends Service {
+    NotificationManager nm;
+    LocationManager locationManager;
+    //LocationListener locationListener;
+    Boolean flag = true;
+    private LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+            Log.i("dadadada", "on statuschanged");
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+            flag=true;
+           sendNotif();
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+            flag=false;
+            sendNotif();
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        Log.i("dadadadada", "onCreate");
+        super.onCreate();
+        nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i("dadadadada", "onStartCommand");
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                1000 * 10, 10, locationListener);
+        sendNotif();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+    void sendNotif() {
+        Log.i("dadadadada","sendNotif");
+        // 1-я часть
+        Notification notif;
+        if(flag) {
+            notif = new Notification(R.drawable.ic_gps_fix, "Text in status bar",
+                    System.currentTimeMillis());
+        }
+        else {
+            notif = new Notification(R.drawable.ic_gps_unfix, "Text in status bar",
+                    System.currentTimeMillis());
+        }
+
+        // 3-я часть
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra(MainActivity.FILE_NAME, "somefile");
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        // 2-я часть
+       //notif.setLatestEventInfo(this, "Notification's title", "Notification's text", pIntent)
+
+        // ставим флаг, чтобы уведомление пропало после нажатия
+        notif.flags |= Notification.FLAG_AUTO_CANCEL;
+
+        // отправляем
+        nm.notify(1, notif);
+    }
+
+    private void checkEnabled(){
+        //if(locationManager.isProviderEnabled())
+    }
+
+}
